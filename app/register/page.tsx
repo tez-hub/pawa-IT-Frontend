a8fc872236298b177cc8cf0ddbae96e2010cf839
User <dennismwema8@gmail.com>
2025-04-23 03:40:36 +0300

    register page fixed

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -3,6 +3,7 @@
 import { FormEvent, useState } from "react";
 import { useRouter } from "next/navigation";
 import { registerUser } from "@/apiCalls/api";
+import Link from "next/link";
 
 export default function Register() {
   const [email, setEmail] = useState("");
@@ -20,27 +21,36 @@ export default function Register() {
   };
 
   return (
-    <div className="p-8">
-      <h1 className="text-2xl font-bold mb-4">Register</h1>
-      <form onSubmit={handleRegister} className="space-y-4">
-        <input
-          className="border p-2 w-full"
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <input
-          className="border p-2 w-full"
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <button className="bg-blue-500 text-white px-4 py-2 rounded">Register</button>
-      </form>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4">
+      <div className="bg-white shadow-lg rounded-xl p-8 w-full max-w-md">
+        <h1 className="text-3xl font-bold text-center text-blue-600 mb-6">Register</h1>
+        <form onSubmit={handleRegister} className="space-y-5">
+          <input
+            className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <input
+            className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <button
+            type="submit"
+            className="w-full bg-blue-600 hover:bg-blue-700 text-white font-semibold py-3 rounded-lg transition duration-200"
+          >
+            Register
+          </button>
+        </form>
+        <p className="text-sm text-black text-center pt-5">Have an account? <Link className="text-blue-600" href={'/login'}>Login</Link></p>
+      </div>
     </div>
+
   );
 }
